95048aa01e85954e73ea4eb6277be98fa0c2ebb8
Roman Shmyrev <rshmyrev@pladform.ru>
2018-10-19 16:16:31 +0300

    init

---
+++ install_mac.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+brew update
+brew install wget
+brew install ffmpeg
+brew install graphviz
+brew install jpeg
+brew install opencv3
+brew install tensorflow
+brew install slim
+brew install python3-tk
+brew install portaudio
+brew install protobuf
+brew install swig
